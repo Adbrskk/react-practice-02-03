c303207ae201f81f91b2697db5731c75f92c5f2d
Adbrskk <aedobroskok@gmail.com>
2026-03-02 13:18:50 +0100

    add comp and pages

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar.jsx";
+import Home from "./pages/Home.jsx";
+import Users from "./pages/Users.jsx";
+import UserProfile from "./pages/UserProfile.jsx";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/users/:userId" element={<UserProfile />} />
+
+        <Route path="*" element={<h2>404: Page not found</h2>} />
+      </Routes>
+    </div>
+  );
+}
